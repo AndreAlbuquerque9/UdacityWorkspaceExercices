f2c62e2343ba4bd779526be10d98f705994679f4
Andre Monteiro <dan.andre91@gmail.com>
2018-09-27 22:04:21 -0300

    Finalização do exercicio 5 - Componentes Controlados

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">ReactND - Coding Practice</h1>
-        </header>
-        <div className="container">
-          <input type="text" placeholder="Say Something" />
-          <p className="echo">Echo:</p>
-          <p>This should mirror the text you typed into the input field.</p>
-        </div>
-      </div>
-    );
-  }
+    state = {
+        value: ""
+    };
+
+    handleChange = event => {
+        this.setState({
+            value: event.target.value
+        });
+    };
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">ReactND - Coding Practice</h1>
+                </header>
+                <div className="container">
+                    <input
+                        type="text"
+                        placeholder="Say Something"
+                        value={this.state.value}
+                        onChange={this.handleChange}
+                    />
+                    <p className="echo">Echo:</p>
+                    <p>
+                        {this.state.value
+                            .split("")
+                            .reverse()
+                            .join("")}
+                    </p>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
